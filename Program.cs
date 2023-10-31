bcdc38ac00fa5842d1019bb6bb7e5ce20f3b6966
Steff Beckers <s.beckers@fuzed.app>
2023-10-31 16:44:27 +0100

    Updates

--- Program.cs
+++ Program.cs
@@ -1,18 +1,18 @@
 ﻿using NodaTime;
 
-DateTime dateTime = DateTime.Today;
+DateTime dateTime = new DateTime(2023, 10, 27, 22, 0, 0, DateTimeKind.Utc);
 Console.WriteLine("dateTime:\t\t\t\t\t" + dateTime);
-Console.WriteLine("dateTime.AddDays(-30):\t\t\t\t" + dateTime.AddDays(-30));
+Console.WriteLine("dateTime.AddDays(30):\t\t\t\t" + dateTime.AddDays(30));
 
 Console.WriteLine();
 
 LocalDateTime localDateTime = LocalDateTime.FromDateTime(dateTime);
 Console.WriteLine("localDateTime:\t\t\t\t\t" + localDateTime);
-Console.WriteLine("localDateTime.PlusDays(-30):\t\t\t" + localDateTime.PlusDays(-30));
+Console.WriteLine("localDateTime.PlusDays(30):\t\t\t" + localDateTime.PlusDays(30));
 
 Console.WriteLine();
 
 DateTimeZone belgium = DateTimeZoneProviders.Tzdb["Europe/Brussels"];
 ZonedDateTime zonedDateTime = localDateTime.InZoneStrictly(belgium);
 Console.WriteLine("zonedDateTime:\t\t\t\t\t" + zonedDateTime);
-Console.WriteLine("zonedDateTime.Plus(Duration.FromDays(-30)):\t" + zonedDateTime.Plus(Duration.FromDays(-30)));
+Console.WriteLine("zonedDateTime.Plus(Duration.FromDays(30)):\t" + zonedDateTime.Plus(Duration.FromDays(30)));
