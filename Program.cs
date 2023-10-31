c9ba7afd90fe9e143a15b3a6f791b19d36da7474
Steff Beckers <s.beckers@fuzed.app>
2023-10-31 15:30:20 +0100

    Tests added

--- Program.cs
+++ Program.cs
@@ -1,4 +1,18 @@
-﻿DateTime dateTime = DateTime.UtcNow;
+﻿using NodaTime;
 
-Console.WriteLine("dateTime:\t\t" + dateTime);
-Console.WriteLine("dateTime.AddDays(-7):\t" + dateTime.AddDays(-7));
+DateTime dateTime = DateTime.Today;
+Console.WriteLine("dateTime:\t\t\t\t\t" + dateTime);
+Console.WriteLine("dateTime.AddDays(-30):\t\t\t\t" + dateTime.AddDays(-30));
+
+Console.WriteLine();
+
+LocalDateTime localDateTime = LocalDateTime.FromDateTime(dateTime);
+Console.WriteLine("localDateTime:\t\t\t\t\t" + localDateTime);
+Console.WriteLine("localDateTime.PlusDays(-30):\t\t\t" + localDateTime.PlusDays(-30));
+
+Console.WriteLine();
+
+DateTimeZone belgium = DateTimeZoneProviders.Tzdb["Europe/Brussels"];
+ZonedDateTime zonedDateTime = localDateTime.InZoneStrictly(belgium);
+Console.WriteLine("zonedDateTime:\t\t\t\t\t" + zonedDateTime);
+Console.WriteLine("zonedDateTime.Plus(Duration.FromDays(-30)):\t" + zonedDateTime.Plus(Duration.FromDays(-30)));
